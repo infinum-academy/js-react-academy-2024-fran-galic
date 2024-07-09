1aa87b0f5d9cef1e40a68b3345748eebafe99512
faks <87184157+fran-galic@users.noreply.github.com>
2024-07-09 16:01:58 +0200

    creates the initial project structure

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # React Academy 2024
 
 React Academy 2024 assignments repository
-
-Proba bato
---
+++ tv-shows-ratting-app/src/app/providers.tsx
@@ -0,0 +1,9 @@
+// za charku
+// app/providers.tsx
+'use client'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider>{children}</ChakraProvider>
+}
