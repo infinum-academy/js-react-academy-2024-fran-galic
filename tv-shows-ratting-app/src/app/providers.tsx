5e357d0418059e2d2887baaf145eab1478b454c5
faks <87184157+fran-galic@users.noreply.github.com>
2024-07-14 14:38:21 +0200

    instalirao SWR

--- tv-shows-ratting-app/src/app/providers.tsx
+++ tv-shows-ratting-app/src/app/providers.tsx
@@ -3,7 +3,13 @@
 'use client'
 
 import { ChakraProvider } from '@chakra-ui/react'
+import { SWRConfig } from 'swr'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return (<SWRConfig>
+            <ChakraProvider>
+              {children}
+            </ChakraProvider>
+          </SWRConfig>)
 }
+
