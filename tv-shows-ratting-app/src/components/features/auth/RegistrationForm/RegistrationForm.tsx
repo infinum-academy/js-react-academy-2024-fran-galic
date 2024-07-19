c9c8482b31042fc785798a4a68b83797030c3a70
faks <87184157+fran-galic@users.noreply.github.com>
2024-07-19 14:05:25 +0200

    poboljsana RegisterForm da smao korsiti stvari iz useForm Hooka za validaicju

--- tv-shows-ratting-app/src/components/features/auth/RegistrationForm/RegistrationForm.tsx
+++ tv-shows-ratting-app/src/components/features/auth/RegistrationForm/RegistrationForm.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 'use client';
+
 import React, { useState } from 'react';
-import { Card, CardBody, Flex, chakra, FormControl, InputGroup, InputLeftElement, Input, Button, Text, FormHelperText, Spinner } from '@chakra-ui/react';
+import { Card, CardBody, Flex, chakra, FormControl, Button, Text, Spinner, FormErrorMessage } from '@chakra-ui/react';
 import { EmailIcon, LockIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
 import { useForm } from 'react-hook-form';
@@ -12,19 +13,14 @@ import { mutator } from '@/fetchers/mutators';
 import { PasswordInput } from '@/components/shared/auth/PasswordInput/PasswordInput';
 import { CustomInput } from '@/components/shared/auth/CustomInput/CustomInput';
 
-
-
 interface IRegistrationFormInputs {
   email: string,
   password: string,
   password_confirmation: string
 }
 
-
 export const RegistrationForm = () => {
-
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<IRegistrationFormInputs>();
-  const [errorMessage, setErrorMessage] = useState("");  // po defoultu je errorMessage "" sto je faoult vrijednost
+  const { register, handleSubmit, formState: { isSubmitting, errors }, setError, clearErrors, getValues } = useForm<IRegistrationFormInputs>();
   const [registered, setRegistered] = useState(false);
 
   const { trigger } = useSWRMutation(swrKeys.register, mutator<IRegistrationFormInputs>,
@@ -32,20 +28,23 @@ export const RegistrationForm = () => {
       onSuccess: () => {
         setRegistered(true);
       },
+      onError: () => {
+        setError("email", { type: "manual", message: "This account already exists" });
+      }
     }
   ); 
 
   const onRegister = async (data: IRegistrationFormInputs) => {
     if (data.password.length < 8) {
-      setErrorMessage("Password must be at least 8 characters");
+      setError("password", { type: "manual", message: "Password must be at least 8 characters" });
       return;
     }
     if (data.password !== data.password_confirmation) {
-      setErrorMessage("Passwords do not match");
+      setError("password_confirmation", { type: "manual", message: "Passwords do not match" });
       return;
     }
-    //ukoliko je sve okej:
-    setErrorMessage("");
+    // Clear previous errors if any
+    clearErrors();
     console.log("On register:", data);
     await trigger(data); 
   };
@@ -66,37 +65,34 @@ export const RegistrationForm = () => {
               gap={8}
               onSubmit={handleSubmit(onRegister)}
             >
-              
               <CustomInput 
-                RegisterPart={register('email')} 
+                RegisterPart={register('email', { required: 'Email is required' })} 
                 isDisabled={isSubmitting} 
                 testId={"email"} 
                 placeholder={'Email'} 
                 icon={<EmailIcon color="white" />} 
+                error={errors.email?.message}
               />
 
               <PasswordInput 
-                RegisterPart={register('password')} 
+                RegisterPart={register('password', { required: 'Password is required', minLength: { value: 8, message: 'Password must be at least 8 characters' } })} 
                 isDisabled={isSubmitting} 
                 testId={"password"} 
                 placeholder={"Password"}
                 icon={<LockIcon color="white" />} 
+                error={errors.password?.message}
               />
 
               <PasswordInput 
-                RegisterPart={register('password_confirmation')} 
+                RegisterPart={register('password_confirmation', { required: 'Password confirmation is required', validate: value => value === getValues('password') || 'Passwords do not match' })} 
                 isDisabled={isSubmitting} 
                 testId={"password_confirmation"} 
                 placeholder={"Confirm password"} 
                 icon={<LockIcon color="white" />} 
+                error={errors.password_confirmation?.message}
               />
 
               <Button type="submit" px={7} borderRadius="20px" fontSize="sm" color="#371687" isDisabled={isSubmitting}>{isSubmitting ? <Spinner /> : 'SIGN UP'}</Button>
-              {errorMessage && (
-                <Text fontSize="sm" color="white">
-                  {errorMessage}
-                </Text>
-              )}
             </chakra.form>
             <Text color="white">Already have an account? <Text as={NextLink} href="/login" fontWeight="bold">Login</Text></Text>
           </Flex>
--- tv-shows-ratting-app/src/components/shared/auth/CustomInput/CustomInput.tsx
+++ tv-shows-ratting-app/src/components/shared/auth/CustomInput/CustomInput.tsx
@@ -1,36 +1,37 @@
 'use client';
-import { FormControl, IconButton, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+
+import { FormControl, FormErrorMessage, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 interface ICustomInputProps {
-   RegisterPart: any,
-   isDisabled: boolean,
-   testId: string;
-   placeholder: string;
-   icon: any;
+  RegisterPart: any,
+  isDisabled: boolean,
+  testId: string;
+  placeholder: string;
+  icon: any;
+  error?: string;
 }
 
 export const CustomInput = (props: ICustomInputProps) => {
-
-   return (
-      <FormControl isRequired>
-         <InputGroup size='md'>
-            <InputLeftElement pointerEvents="none" >
-            {props.icon}
-            </InputLeftElement>
-            <Input
-               type='text'
-               placeholder={props.placeholder}
-               required
-               borderRadius="20px"
-               pl="10"
-               color="white"
-               _placeholder={{ color: 'white' }}
-               {...props.RegisterPart}
-               isDisabled={props.isDisabled}
-               data-testid={props.testId}
-            />
-         </InputGroup> 
-      </FormControl>
-   );
-
+  return (
+    <FormControl isRequired isInvalid={!!props.error}>
+      <InputGroup size='md'>
+        <InputLeftElement pointerEvents="none">
+          {props.icon}
+        </InputLeftElement>
+        <Input
+          type='text'
+          placeholder={props.placeholder}
+          required
+          borderRadius="20px"
+          pl="10"
+          color="white"
+          _placeholder={{ color: 'white' }}
+          {...props.RegisterPart}
+          isDisabled={props.isDisabled}
+          data-testid={props.testId}
+        />
+      </InputGroup>
+      {props.error && <FormErrorMessage>{props.error}</FormErrorMessage>}
+    </FormControl>
+  );
 }
