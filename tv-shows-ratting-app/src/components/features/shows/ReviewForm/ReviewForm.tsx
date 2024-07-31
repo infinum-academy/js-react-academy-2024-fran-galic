14154c076a83dbede4656ebe4002237db11a7ee1
faks <87184157+fran-galic@users.noreply.github.com>
2024-07-31 19:51:18 +0200

    Napravljeno da ReviewForm za StarRating korsiti useForm controller

--- tv-shows-ratting-app/src/components/features/shows/ReviewForm/ReviewForm.tsx
+++ tv-shows-ratting-app/src/components/features/shows/ReviewForm/ReviewForm.tsx
@@ -2,13 +2,13 @@
 
 import React, { useState } from 'react';
 import { Box, Button, Input, Stack, Textarea, Text, Flex, Spinner, FormControl, FormErrorMessage } from "@chakra-ui/react";
-import { StarRating } from "../../review/StarRating/StarRating";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { ICreateReviewData, IReview } from "@/typings/Review.type";
 import useSWRMutation from 'swr/mutation';
 import { swrKeys } from '@/fetchers/swrKeys';
 import { postReview } from '@/mutation/reviews';
 import { mutate } from 'swr';
+import { StarRating } from "../../review/StarRating/StarRating";
 
 interface IReviewFormProps {
   show_id: string
@@ -21,35 +21,17 @@ interface IReviewFormInputs {
 
 export const ReviewForm = ({ show_id }: IReviewFormProps) => {
 
-  //stanja za zvijezde
-  const [isLocked, setLocked] = useState(false);
-  const [numSelectedStars, setNumSelectedStars] = useState(0);
-  const [numHoveredStars, setNumHoveredStars] = useState(0);
+  const { control, register, handleSubmit, formState: { errors, isSubmitting }, setValue, clearErrors } = useForm<IReviewFormInputs>();
 
-  const { register, handleSubmit, setValue, formState: { errors, isSubmitting }, clearErrors } = useForm<IReviewFormInputs>();
-
-  //dio za useWSRMuation
   const { trigger } = useSWRMutation(swrKeys.reviews , postReview,
     {
       onSuccess: () => {
-        //ukoliko si uspio postat na server novi Review, pozovi mutate koji ce revliditirat stvari u zajednickom SWR cashu i samim time ce se stranica re Renderat
         console.log("Reviews se revaliditiraju");
         mutate(swrKeys.allReviews(show_id, 1, 100));
       },
     }
-  ); 
+  );
 
-  //funkcije za star rewiew
-  const onClick = (index: number) => {
-    setNumSelectedStars(index);
-    setValue('grade', index); 
-    clearErrors('grade');
-  }
-  const onHover = (index: number) => {
-    setNumHoveredStars(index);
-  }
-
-  // 2. dio za useWSRMuation
   const addShowReview = async (data: IReviewFormInputs) => {
     const newReviewData: ICreateReviewData = {
       comment: data.description,
@@ -58,7 +40,6 @@ export const ReviewForm = ({ show_id }: IReviewFormProps) => {
     };
     
     setValue('grade', 0);
-    setNumSelectedStars(0);
     setValue('description', "");
 
     await trigger(newReviewData);
@@ -82,7 +63,7 @@ export const ReviewForm = ({ show_id }: IReviewFormProps) => {
           pt={7}
           _placeholder={{ color: 'purple' }}
           borderColor="purple"
-          borderWidth={2}  // Dodano svojstvo za deblji obrub
+          borderWidth={2}
           _hover={{ borderColor: "purple" }}
           _focus={{ borderColor: "purple", boxShadow: 'none' }}
           _invalid={{ borderColor: "pink" }}
@@ -92,24 +73,34 @@ export const ReviewForm = ({ show_id }: IReviewFormProps) => {
 
       <Flex justify={"space-between"} pl={{base: 0, sm: 10}} pt={2} align={"center"}>
         <FormControl isInvalid={!!errors.grade}>
-            <Flex gap={4} align="baseline">
-              <Text>Rating</Text>
-              <Box maxWidth="105px" onMouseLeave={() => setLocked(true)} onMouseEnter={() => setLocked(false)}>
-                <StarRating 
-                  noOfStars={isLocked ? numSelectedStars : numHoveredStars} 
-                  isStatic={isSubmitting} 
-                  onClick={onClick} 
-                  onHover={onHover} 
-                  data_testid="star-rating"
-                />
-              </Box>
-            </Flex>
-            {errors.grade && <FormErrorMessage color="pink">{errors.grade.message}</FormErrorMessage>}
-            <Input type="hidden" {...register('grade', { 
-              required: 'Rating is required', 
-              validate: value => value > 0 || 'You must select a rating'
-            })} /> {/* hidden input za rating */}
-          </FormControl>
+          <Flex gap={4} align="baseline">
+            <Text>Rating</Text>
+            <Box maxWidth="105px">
+              <Controller
+                name="grade"
+                control={control}
+                defaultValue={0}
+                rules={{ 
+                  required: 'Rating is required', 
+                  validate: value => value > 0 || 'You must select a rating'
+                }}
+                render={({ field }) => (
+                  <StarRating
+                    noOfStars={field.value}
+                    isStatic={isSubmitting}
+                    onClick={(index) => {
+                      field.onChange(index);
+                      clearErrors('grade');
+                    }}
+                    onHover={() => {}}
+                    data_testid="star-rating"
+                  />
+                )}
+              />
+            </Box>
+          </Flex>
+          <FormErrorMessage color="pink">{errors.grade?.message}</FormErrorMessage>
+        </FormControl>
 
         <Button 
           type="submit" 
