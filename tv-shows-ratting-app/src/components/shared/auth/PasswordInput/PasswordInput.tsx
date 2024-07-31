126504c1f52744dc91c4e5e3f058d50dd864e17d
faks <87184157+fran-galic@users.noreply.github.com>
2024-07-31 17:14:37 +0200

    poboljsane CustomInput i PasswordInput komponete da rade po kriteriju 2. zadatka, i ispravljeno njeno koristenje u Form elementima

--- tv-shows-ratting-app/src/components/shared/auth/PasswordInput/PasswordInput.tsx
+++ tv-shows-ratting-app/src/components/shared/auth/PasswordInput/PasswordInput.tsx
@@ -1,146 +1,15 @@
-/* 'use client';
+'use client';
 
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { FormControl, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, FormErrorMessage } from "@chakra-ui/react";
+import { FormControl, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, FormErrorMessage, InputProps, forwardRef } from "@chakra-ui/react";
 import { useState } from "react";
-
-interface IPasswordInputProps {
-  RegisterPart: any,
-  isDisabled: boolean,
-  testId: string;
-  placeholder: string;
-  icon: any;
-  error?: string;
-}
-
-export const PasswordInput = (props: IPasswordInputProps) => {
-  const [isHidden, setHidden] = useState(true);
-
-  return (
-    <FormControl isRequired isInvalid={!!props.error} maxWidth={"300px"}>
-      <InputGroup size='md'>
-        <InputLeftElement pointerEvents="none">
-          {props.icon}
-        </InputLeftElement>
-        <Input
-          type={isHidden ? 'password' : 'text'}
-          placeholder={props.placeholder}
-          required
-          borderRadius={1}
-          pl="10"
-          color="white"
-          _placeholder={{ color: 'white' }}
-          borderColor={props.error ? "pink" : "white"}
-          borderWidth={2}  // Dodano svojstvo za deblji obrub
-          _hover={{ borderColor: props.error ? "pink" : "white" }}
-          _focus={{ borderColor: props.error ? "pink" : "white", boxShadow: 'none' }}
-          _invalid={{ borderColor: "pink" }}
-          {...props.RegisterPart}
-          isDisabled={props.isDisabled}
-          data-testid={props.testId}
-          fontSize={5}
-          fontWeight={"normal"}
-          maxWidth={"300px"}
-        />
-        <InputRightElement fontSize="md">
-          <IconButton
-            color="white"
-            aria-label="toggle view"
-            icon={isHidden ? <ViewIcon /> : <ViewOffIcon />}
-            onClick={() => {
-              setHidden(!isHidden);
-            }}
-            backgroundColor="transparent"
-            _hover={{ backgroundColor: 'transparent' }}
-          />
-        </InputRightElement>
-      </InputGroup>
-      {props.error && <FormErrorMessage color="pink">{props.error}</FormErrorMessage>}
-    </FormControl>
-  );
-} */
-
-
-
-
-  'use client';
-
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { FormControl, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, FormErrorMessage, InputProps } from "@chakra-ui/react";
-import { forwardRef, useState } from "react";
-
-interface IPasswordInputProps {
-  RegisterPart: any,
-  isDisabled: boolean,
-  testId: string;
-  placeholder: string;
-  icon: any;
-  error?: string;
-}
 
 interface IPasswordInput extends InputProps {
   name: string;
-  placeholder: string;
   testId: string;
   icon: any;
   error?: string;
 }
-
-
-/* 
- export const PasswordInput = (props: IPasswordInputProps) => {
-  const [isHidden, setHidden] = useState(true);
-
-  return (
-    <FormControl isRequired isInvalid={!!props.error} maxWidth={"300px"}>
-      <InputGroup size='md'>
-        <InputLeftElement pointerEvents="none">
-          {props.icon}
-        </InputLeftElement>
-        <Input
-          type={isHidden ? 'password' : 'text'}
-          placeholder={props.placeholder}
-          required
-          borderRadius={1}
-          pl="10"
-          color="white"
-          _placeholder={{ color: 'white' }}
-          borderColor={props.error ? "pink" : "white"}
-          borderWidth={2}  // Dodano svojstvo za deblji obrub
-          _hover={{ borderColor: props.error ? "pink" : "white" }}
-          _focus={{ borderColor: props.error ? "pink" : "white", boxShadow: 'none' }}
-          _invalid={{ borderColor: "pink" }}
-          {...props.RegisterPart}
-          isDisabled={props.isDisabled}
-          data-testid={props.testId}
-          fontSize={5}
-          fontWeight={"normal"}
-          maxWidth={"300px"}
-        />
-        <InputRightElement fontSize="md">
-          <IconButton
-            color="white"
-            aria-label="toggle view"
-            icon={isHidden ? <ViewIcon /> : <ViewOffIcon />}
-            onClick={() => {
-              setHidden(!isHidden);
-            }}
-            backgroundColor="transparent"
-            _hover={{ backgroundColor: 'transparent' }}
-          />
-        </InputRightElement>
-      </InputGroup>
-      {props.error && <FormErrorMessage color="pink">{props.error}</FormErrorMessage>}
-    </FormControl>
-  );
-}
-
-
-
- */
- 
-
-
 
 export const PasswordInput = forwardRef(
   ({ placeholder, testId, icon, error, ...rest }: IPasswordInput, ref) => {
